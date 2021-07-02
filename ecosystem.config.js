072c87be9cc84306d897bbaa924d115ad5e6b4cc
Elder Vivot <eldervivot@gmail.com>
2021-07-01 22:43:56 -0300

    feat: add cron_restar, min_uptime, max_restarts, restart_delay

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,32 +4,55 @@ module.exports = {
       name: 'bay-extracts-node',
       cwd: 'C:\\iacon\\baymax-extracts-node',
       script: 'dist\\schedules\\server.js',
+      cron_restart: "1 0 * * *",
+      min_uptime: 5000,
+	    max_restarts: 3,
+	    restart_delay: 4000
     },
     {
       name: 'iacon-rest-api-pg',
       cwd: 'C:\\iacon\\iacon-rest-api-pg',
-      script: 'dist\\server.js'
+      script: 'dist\\server.js',
+      cron_restart: "1 0 * * *",
+      min_uptime: 5000,
+	    max_restarts: 3,
+	    restart_delay: 4000
     },
     {
       name: 'webscraping-nfe-go-s',
       cwd: 'C:\\iacon\\webscraping-nfe-nfce-go',
-      script: 'dist\\schedules\\server.js'
+      script: 'dist\\schedules\\server.js',
+      cron_restart: "1 0 * * *",
+      min_uptime: 5000,
+	    max_restarts: 3,
+	    restart_delay: 4000
     },
     {
       name: 'webscraping-nfe-go-q',
       cwd: 'C:\\iacon\\webscraping-nfe-nfce-go',
       script: 'dist\\queues\\queue.js',
-      cron_restart: "0 */1 * * *"	
+      cron_restart: "0 */1 * * *",
+      min_uptime: 5000,
+	    max_restarts: 3,
+	    restart_delay: 4000
     },
     {
       name: 'webscraping-gyn-go-s',
       cwd: 'C:\\iacon\\webscraping-nfse-goiania',
-      script: 'dist\\schedules\\server.js'
+      script: 'dist\\schedules\\server.js',
+      cron_restart: "1 0 * * *",
+      min_uptime: 5000,
+	    max_restarts: 3,
+	    restart_delay: 4000
     },
     {
       name: 'webscraping-gyn-go-q',
       cwd: 'C:\\iacon\\webscraping-nfse-goiania',
-      script: 'dist\\queues\\queue.js'
+      script: 'dist\\queues\\queue.js',
+      cron_restart: "1 0 * * *",
+      min_uptime: 5000,
+	    max_restarts: 3,
+	    restart_delay: 4000
     }
   ],
 
